f47b522fffbd129abf9a63c770457bb48f922c8a
cliedeman <ciaranliedeman@gmail.com>
2018-10-05 14:06:11 +0200

    Added more react code

--- src/App.tsx
+++ src/App.tsx
@@ -1,7 +1,12 @@
-import * as React from 'react';
+import React from 'react';
+import ReactDOM from 'react-dom';
+
+const Child = () => <div>child</div>;
 
 class App extends React.Component {
   render() {
     return null;
   }
 }
+
+ReactDOM.render(<App />, document.getElementById('root'));
